d31faa1b9f8f124f95026ac682813eb8c3b83f68
Ameya Kulkarni <ameyak2001@gmail.com>
2023-12-15 01:55:28 +0530

    minor changes

--- JavaScript/Webserver/library1.js
+++ JavaScript/Webserver/library1.js
@@ -3,6 +3,6 @@ function reverse(str1, cbf1) {
     for (let i = str1.length - 1; i >= 0; i--) {
         reversestr += str1.charAt(i)
     }
-    return reversestr
+    setTimeout(cbf1(reversestr), 2000)
 }
 module.exports = reverse;
